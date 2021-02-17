e28afc0cf26cfd06835effd6bf73b9ec394c7af7
fabianorf <freis884@gmail.com>
2021-02-17 16:39:37 -0300

    testes com draggable de sortable

---
+++ script.js
@@ -0,0 +1,17 @@
+const campos = {
+  um: [2, 4],
+  dois: [1, 3, 5],
+  tres: []
+}
+
+const vizinhos = [[1, 3], [1, 3, 5], [2, 6], [1, 5, 7]]
+
+vizinhos[7];
+
+if(pos===1 ){
+  variavel = [1, 3]
+}
+
+
+
+console.log(7)
